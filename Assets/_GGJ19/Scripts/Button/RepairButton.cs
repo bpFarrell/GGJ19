fc2e0b5d72a1593c4ae4aadfdeeb760b740c36d3
brandon farrell <brandonf@bentimagelab.com>
2019-01-26 13:35:39 -0800

    Basic Repair.

---
+++ Assets/_GGJ19/Scripts/Button/RepairButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairButton : BaseButton
+{
+    public override void Interact() {
+        Destroy(gameObject);
+    }
+
+    public override void OnEnter() {
+
+    }
+
+    public override void OnLeave() {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
