f78a81f92dcd16fc50cbdf4a7a6dc0074026b442
brandon farrell <brandonf@bentimagelab.com>
2019-01-26 12:18:45 -0800

    ShipTempalte

---
+++ Assets/_GGJ19/Scripts/Level/DockedShip.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DockedShip : MonoBehaviour
+{
+    public float teleportTime = 30;
+    public float blueTicSec = 0;
+    public float redTicSec = 0;
+    public float greenTicSec = 0;
+
+}
