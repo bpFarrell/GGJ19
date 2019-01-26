60d8a95b0af0581a7e47b80507d18e5a1d08f39d
JustJess <dev.justjess@gmail.com>
2019-01-26 13:26:09 -0800

    Initial audio push
    Scripts currently non-functional, just want to sync to current

---
+++ Assets/_GGJ19/Scripts/Audio/InteractAudioController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class InteractAudioController : MonoBehaviour
+{
+    //private bool buttonPushed;
+    BaseButton debugButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject button = GameObject.Find("Button");
+        DebugButton debugButton = button.GetComponent<DebugButton>();
+        debugButton.Interact();
+        {
+            Debug.Log("Fire the button audio!");
+        }
+    }
+
+    void OnInteract()
+    {
+
+    }
+}
