510fef1700e85b268a0714611775f64e518679b5
brandon farrell <brandonf@bentimagelab.com>
2019-01-26 01:24:36 -0800

    Quick Test room

---
+++ Assets/_GGJ19/Scripts/TempCameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempCameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    private void Awake() {
+        offset = transform.position- target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position + offset;
+    }
+}
