ca53b5922426ad8b29662cc06a73ff6cbe1833e4
Jason Dean Crossley <jasoncr@bentimagelab.com>
2019-01-27 15:28:34 -0800

    MeshRenderer button for new Material for Brandon

---
+++ Assets/_GGJ19/Scripts/Editor/MeshRendererEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MeshRenderer))]
+public class MeshRendererEditor : Editor
+{
+    private static bool foldout = true;
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        MeshRenderer script = (MeshRenderer)target;
+
+        if (foldout = EditorGUILayout.Foldout(foldout, "Debug"))
+        {
+            if (GUILayout.Button("New Material"))
+            {
+                Material mat = new Material(Shader.Find("Standard"));
+                AssetDatabase.CreateAsset(mat, "Assets/New Material.mat");
+                script.material = mat;
+            }
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUI.EndDisabledGroup();
+        }
+    }
+}
