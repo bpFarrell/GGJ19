bcf4e27d21134f2fab1c68fba22da80a60a78d16
Jason Dean Crossley <jasoncr@bentimagelab.com>
2019-01-26 20:48:26 -0800

    Disabled parts of door triggers that aren't ready to be implemented yet.

--- Assets/_GGJ19/Scripts/DoorTrigger.cs
+++ Assets/_GGJ19/Scripts/DoorTrigger.cs
@@ -1,27 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class DoorTrigger : BaseButton
 {
-    public RoomNode doorOneSide;
-    public RoomNode doorOtherSide;
+    public GameObject door;
+    public Vector3 start;
 
-    public GameObject doorPanel1, doorPanel2; //these slide open and closed
-    float slidePos; //0..1
-    enum Sliding { None, Open, Closed }
+    public AudioSource soundOpen;
+    public AudioSource soundClose;
+
+    private float slidePos; //0..1
+
+    enum Sliding {
+        NONE,
+        OPEN,
+        CLOSED
+    }
     Sliding sliding;
 
-    public override void OnEnter()
-    {
-        Debug.Log("Enter door trigger for " + (doorOneSide == null ? "null" : doorOneSide.name) + " to " + (doorOtherSide == null ? "null" : doorOtherSide.name));
-        sliding = Sliding.Open;
+    public void Initialize() {
+        start = door.transform.position;
+    }
+    private void Cleanup() {
+
+    }
+    public void Update() {
+        switch (sliding)
+        {
+            case Sliding.NONE:
+                door.transform.position = start;
+                break;
+            case Sliding.OPEN:
+                door.transform.position = Vector3.Lerp(door.transform.position, start + (door.transform.right * 2), 0.4f);
+                break;
+            case Sliding.CLOSED:
+                door.transform.position = Vector3.Lerp(door.transform.position, start, 0.7f);
+                break;
+            default:
+                break;
+        }
     }
 
-    public override void OnLeave()
-    {
-        Debug.Log("Leave door trigger for " + (doorOneSide == null ? "null" : doorOneSide.name) + " to " + (doorOtherSide == null ? "null" : doorOtherSide.name));
-        sliding = Sliding.Closed;
+    public override void OnEnter() {
+        Debug.Log("Enter door trigger");
+        sliding = Sliding.OPEN;
+    }
+
+    public override void OnLeave() {
+        Debug.Log("Leave door trigger");
+        sliding = Sliding.CLOSED;
     }
 
     public override void Interact() { }
